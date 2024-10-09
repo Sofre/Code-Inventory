66eced6a6bfc0114834944e14f1cf346f7cd334b
Dushko Sofronievski <142673399+Sofre@users.noreply.github.com>
2024-10-10 00:05:21 +0200

    Delete Sum.py

--- Sum.py
+++
@@ -1,8 +0,0 @@
-first_number = input("Enter first number ")
-second_number = input("Enter second number ")
-
-first_number = int(first_number)
-second_number = int(second_number)
-
-print(f"The sum of {first_number} and the {second_number} is {first_number+second_number}")
-
