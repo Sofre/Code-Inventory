7f3d1f6d41ad455d125a6539c6a95162addccea2
Dusko_Sofronievski <dukisofronievski@gmail.com>
2024-12-16 19:57:12 +0100

    Commit

--- Python exercises/Binary Tree BLACKBOX.py
+++ Python exercises/Binary Tree BLACKBOX.py
@@ -1,48 +1,42 @@
+
 class Node:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
+
 class BinaryTree:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, value):
-        if self.root is None:
-            self.root = Node(value)
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+  
+    def insert_left(self, new_node):
+        if self.left is None:
+            self.left = BinaryTree(new_node)
         else:
-            self._insert(self.root, value)
-
-    def _insert(self, node, value):
-        if value < node.value:
-            if node.left is None:
-                node.left = Node(value)
-            else:
-                self._insert(node.left, value)
+            # Inserting new value where it pushes old value as the child of the new value
+            t = BinaryTree(new_node)
+            t.left = self.left # gives old value to new child
+            self.left = t # new value is given to left child
+    
+    def insert_right(self, new_node):
+        # Same as insert_left
+        if self.right is None:
+            self.right = BinaryTree(new_node)
         else:
-            if node.right is None:
-                node.right = Node(value)
-            else:
-                self._insert(node.right, value)
+            t = BinaryTree(new_node)
+            t.right = self.right
+            self.right = t
+        
+    def print_tree(self, level=0, prefix="Root: ", spacing="    "):
+        
+        print(f"{spacing * level}{prefix}{self.key}")
+        
+    
+        if self.left:
+            self.left.print_tree(level + 1, "L--- ", spacing)
+        if self.right:
+            self.right.print_tree(level + 1, "R--- ", spacing)
 
-    def traverse(self):
-        if self.root is not None:
-            self._traverse(self.root)
-
-    def _traverse(self, node):
-        if node is not None:
-            self._traverse(node.left)
-            print(node.value)
-            self._traverse(node.right)
-
-# Example usage:
-tree = BinaryTree()
-tree.insert(5)
-tree.insert(3)
-tree.insert(7)
-tree.insert(2)
-tree.insert(4)
-tree.insert(6)
-tree.insert(8)
-tree.traverse()
